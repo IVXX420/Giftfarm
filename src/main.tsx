652e2e988fb1074fea22a167e999856442e8f00d
IVXX420 <154575607+IVXX420@users.noreply.github.com>
2025-03-13 04:17:05 +0300

    Обновил URL манифеста TON Connect

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { initTonConnect } from './config/ton';
 // Инициализируем TON Connect
 initTonConnect().catch(console.error);
 
-const manifestUrl = 'https://giftfarm.vercel.app/tonconnect-manifest.json';
+const manifestUrl = 'https://votipapa-fegjqf3nr-ffffffs-projects-46aa7d61.vercel.app/ton-connect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
